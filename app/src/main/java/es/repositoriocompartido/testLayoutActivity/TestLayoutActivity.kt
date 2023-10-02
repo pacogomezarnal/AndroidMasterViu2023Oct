017ac40b6f9f6ac97d5e4174c6e2f813f3c183e2
Paco Mastermind <paco@mastermind.ac>
2023-10-02 21:58:11 +0200

    LinearLayout

---
+++ app/src/main/java/es/repositoriocompartido/testLayoutActivity/TestLayoutActivity.kt
@@ -0,0 +1,12 @@
+package es.repositoriocompartido.testLayoutActivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import es.repositoriocompartido.androidmasterviu.R
+
+class testLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_layout)
+    }
+}
